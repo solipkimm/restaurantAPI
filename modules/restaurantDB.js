9b0a6d1da2e3a72af793995a6d5809c359716908
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-02-01 14:43:44 -0500

    descriptions of functions

--- modules/restaurantDB.js
+++ modules/restaurantDB.js
@@ -26,6 +26,7 @@ module.exports = class RestaurantDB{
         this.Restaurant = null; // no "Restaurant" object until "initialize" is complete
     }
 
+    // Establish a connection with the MongoDB server and initialize the "Restaurant" model with the "restaurant" collection
     initialize(){
         return new Promise((resolve,reject)=>{
             let db = mongoose.createConnection(this.connectionString,{ useNewUrlParser: true,useUnifiedTopology: true });
@@ -40,12 +41,15 @@ module.exports = class RestaurantDB{
         });
     }
 
+    // Create a new restaurant in the collection using the object passed in the "data" parameter
     async addNewRestaurant(data){
         let newRestaurant = new this.Restaurant(data);
         await newRestaurant.save();
         return `new restaurant: ${newRestaurant._id} successfully added`
     }
     
+    // Return an array of all restaurants for a specific page (sorted by restaurant_id), given the number of items per page.
+    // an optional parameter "borough" that can be used to filter results by a specific "borough" value
     getAllRestaurants(page, perPage, borough){ 
         let findBy = borough ? { borough } : {};
 
@@ -56,15 +60,18 @@ module.exports = class RestaurantDB{
         return Promise.reject(new Error('page and perPage query parameters must be present'));
     }
 
+    // Return a single restaurant object whose "_id" value matches the "Id" parameter
     getRestaurantById(id){
         return this.Restaurant.findOne({_id: id}).exec();
     }
 
+    // Overwrite an existing restaurant whose "_id" value matches the "Id" parameter, using the object passed in the "data" parameter
     async updateRestaurantById(data, id){
         await this.Restaurant.updateOne({_id: id}, { $set: data }).exec();
         return `restaurant ${id} successfully updated`;
     }
 
+    // Delete an existing restaurant whose "_id" value matches the "Id" parameter
     async deleteRestaurantById(id){
         await this.Restaurant.deleteOne({_id: id}).exec();
         return `restaurant ${id} successfully deleted`;
