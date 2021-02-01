d96bfeee3c907a692d711a87cdb8404d8bbef5ba
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-02-01 14:13:53 -0500

    add profile

---
+++ server.js
@@ -0,0 +1,11 @@
+/*********************************************************************************
+* WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Solip Kim    Student ID: 120618194    Date: Jan 22 2021
+* Heroku Link: https://cryptic-lowlands-55609.herokuapp.com/
+*
+********************************************************************************/
+
