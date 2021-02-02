70d5d52baedb1df3fad0133a4cab1e44e610dc88
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-02-02 16:54:42 -0500

    add function avg

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ let currentRestaurant = {};
 let page = 1;
 let perPage = 10;
 let map = null;
+
+function avg(grades){
+  let total = 0;
+
+  if (grades.length > 0){
+    for (let i = 0; i < grades.length; i++){
+      total += element.grade;
+    }
+  }
+  return (total/grades.length).toFixed(2);
+}
